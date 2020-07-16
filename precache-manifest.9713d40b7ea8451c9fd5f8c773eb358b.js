9646f724a50464d6f1d3bfe388b9f8582cec13bb
cozie <vvictor.llin@gmail.com>
2020-07-15 19:39:28 -0700

    Updates

---
+++ precache-manifest.9713d40b7ea8451c9fd5f8c773eb358b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07db6c19ee1e1a7b1c8e2298e9685fde",
+    "url": "/UmpCastV2-Frontend/index.html"
+  },
+  {
+    "revision": "8b057a89af6f659c3deb",
+    "url": "/UmpCastV2-Frontend/static/css/2.a7c91b2e.chunk.css"
+  },
+  {
+    "revision": "0f5fa14ac7bb6b6a7260",
+    "url": "/UmpCastV2-Frontend/static/css/main.fb610f6d.chunk.css"
+  },
+  {
+    "revision": "8b057a89af6f659c3deb",
+    "url": "/UmpCastV2-Frontend/static/js/2.4393c4da.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/UmpCastV2-Frontend/static/js/2.4393c4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5fa14ac7bb6b6a7260",
+    "url": "/UmpCastV2-Frontend/static/js/main.eda1b7cb.chunk.js"
+  },
+  {
+    "revision": "7bcd4c0df0b26d5387b8",
+    "url": "/UmpCastV2-Frontend/static/js/runtime-main.0194c977.js"
+  }
+]);
